3139249dff68a264ba915899bff3a6546a350060
Alexandru <eugenalx@gmail.com>
2023-07-01 20:30:43 -0700

    istall font awesome and navbar

---
+++ gasketClient/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { faCartShopping, faSignOut, faSignIn } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss']
+})
+export class NavBarComponent {
+  faCartShopping = faCartShopping;
+  faSignOut = faSignOut;
+  faSignIn = faSignIn;
+
+}
