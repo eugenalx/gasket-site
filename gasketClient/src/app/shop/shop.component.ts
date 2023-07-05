738342eb96b029db5220f9a4ae9700fe67116653
Alexandru <eugenalx@gmail.com>
2023-07-04 17:06:36 -0700

    filter-sort-searc-pagination

--- gasketClient/src/app/shop/shop.component.ts
+++ gasketClient/src/app/shop/shop.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ShopService } from './shop.service';
 import { Product } from '../shared/models/products';
 import { Brand } from '../shared/models/brand';
 import { Type } from '../shared/models/type';
+import { ShopParams } from '../shared/models/shopParams';
 
 @Component({
   selector: 'app-shop',
@@ -10,17 +11,17 @@ import { Type } from '../shared/models/type';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
+  @ViewChild('search') searchTerm? : ElementRef;
   products: Product [] = [];
   brands: Brand[] = [];
   types: Type[] = [];
-  brandIdSelected = 0;
-  typeIdSelected = 0;
-  sortSelected = "name";
+  shopParams = new ShopParams();
   sortOptions = [
     { name: "Alphabetical" , value:"name" },
     { name: "Price: Low to high" , value:"priceAsc" },
     { name: "Price:High to low" , value:"priceDesc" },
-  ]
+  ];
+  totalCount = 0;
 
   constructor(private shopService: ShopService) {}
   ngOnInit(): void {
@@ -31,38 +32,61 @@ export class ShopComponent implements OnInit {
   }
 
   getProducts() {
-    this.shopService.getProducts(this.brandIdSelected,this. typeIdSelected, this. sortSelected).subscribe({
-      next: response => this.products = response.data,
+    this.shopService.getProducts(this.shopParams).subscribe({
+      next: response => {
+        this.products = response.data;
+        this.shopParams.pageNumber = response.pageIndex;
+        this.shopParams.pageSize = response.pageSize;
+        this.totalCount = response.count;
+      },
       error: error => console.error(error)
     })
   }
 
   getBrands() {
     this.shopService.getBrands().subscribe({
-      next: response => this.brands = [{ id:0,name: 'All'}, ...response],
+      next: response => this.brands = [{ id:0, name: 'All'}, ...response],
       error: error => console.error(error)
     })
   }
 
   getTypes() {
     this.shopService.getTypes().subscribe({
-      next: response => this.types =  [{ id:0,name: 'All'}, ...response],
+      next: response => this.types =  [{ id:0, name: 'All'}, ...response],
       error: error => console.error(error)
     })
   }
 
   onBrandSelected(brandId: number){
-    this.brandIdSelected = brandId;
+    this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber=1;
     this.getProducts();
   }
 
   onTypeSelected(typeId: number){
-    this.typeIdSelected = typeId;
+    this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber=1;
     this.getProducts();
   }
 
    onSortSelected(event: any) {
-    this.sortSelected = event.target.value;
+    this.shopParams.sort = event.target.value;
+    this.getProducts();
+   }
+   onPageChanged(event: any) {
+    if(this.shopParams.pageNumber !== event){
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
+   }
+   onSearch() {
+    this.shopParams.search = this.searchTerm?.nativeElement.value;
+    this.shopParams.pageNumber=1;
+    this.getProducts();
+   }
+   onReset(){
+    if (this.searchTerm) this.searchTerm.nativeElement.value = '';
+    this.shopParams = new ShopParams();
     this.getProducts();
    }
 }
